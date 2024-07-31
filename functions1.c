c90e4f27b721aa96c3559e5aca6ea634e7a483cf
root <schimea@hotmail.com>
2024-07-31 13:41:12 -0700

    Remove betty errors

--- functions1.c
+++ functions1.c
@@ -11,28 +11,29 @@
  * @size: Size specifier
  * Return: Number of chars printed.
  */
+
 int print_unsigned(va_list types, char buffer[],
-  int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
-  int i = BUFF_SIZE - 2;
-  unsigned long int num = va_arg(types, unsigned long int);
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(types, unsigned long int);
 
-  num = convert_size_unsgnd(num, size);
+	num = convert_size_unsgnd(num, size);
 
-  if (num == 0)
-    buffer[i--] = '0';
+	if (num == 0)
+		buffer[i--] = '0';
 
-  buffer[BUFF_SIZE - 1] = '\0';
+	buffer[BUFF_SIZE - 1] = '\0';
 
-  while (num > 0)
-  {
-    buffer[i--] = (num % 10) + '0';
-    num /= 10;
-  }
+	while (num > 0)
+	{
+		buffer[i--] = (num % 10) + '0';
+		num /= 10;
+	}
 
-  i++;
+	i++;
 
-  return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 }
 
 /************* PRINT UNSIGNED NUMBER IN OCTAL  ****************/
@@ -46,34 +47,35 @@ int print_unsigned(va_list types, char buffer[],
  * @size: Size specifier
  * Return: Number of chars printed.
  */
+
 int print_octal(va_list types, char buffer[],
-  int flags, int width, int precision, int size)
+
+	int flags, int width, int precision, int size)
 {
-  int i = BUFF_SIZE - 2;
-  unsigned long int num = va_arg(types, unsigned long int);
-  unsigned long int init_num = num;
+      	int i = BUFF_SIZE - 2;
+      	unsigned long int num = va_arg(types, unsigned long int);
+      	unsigned long int init_num = num;
 
-  UNUSED(width);
+      	UNUSED(width);
 
-  num = convert_size_unsgnd(num, size);
+      	num = convert_size_unsgnd(num, size);
 
-  if (num == 0)
-    buffer[i--] = '0';
+      	if (num == 0)
+	    	buffer[i--] = '0';
 
-  buffer[BUFF_SIZE - 1] = '\0';
+      	buffer[BUFF_SIZE - 1] = '\0';
 
-  while (num > 0)
-  {
-    buffer[i--] = (num % 8) + '0';
-    num /= 8;
-  }
+      	while (num > 0)
+      	{
+	    	buffer[i--] = (num % 8) + '0';
+	    	num /= 8;
+      	}
 
-  if (flags & F_HASH && init_num != 0)
-    buffer[i--] = '0';
+      	if (flags & F_HASH && init_num != 0)
+	    	buffer[i--] = '0';
+      	i++;
 
-  i++;
-
-  return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+      	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 }
 
 /************** PRINT UNSIGNED NUMBER IN HEXADECIMAL **************/
